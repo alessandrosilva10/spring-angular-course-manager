daf9f032e3a2f4968c4b58fbfe1aa9d6acddf4e7
alessandrosilva10 <alessandro.mpgh@gmail.com>
2023-08-04 22:21:21 -0300

    DAO

--- src/main/java/com/javacorner/admin/dao/CourseDAO.java
+++ src/main/java/com/javacorner/admin/dao/CourseDAO.java
@@ -9,7 +9,7 @@ import org.springframework.data.repository.query.Param;
 import com.javacorner.admin.entity.Course;
 
 public interface CourseDAO extends JpaRepository<Course, Long> {
-    List<Course> findByCoursesByCourseNameContains(String keyword);
+    List<Course> findCoursesByCourseNameContains(String keyword);
 
     @Query(value = "select * from tb_courses as c where c.course_id in (select e.course_id from enrolled_in as e where e.student_id=:studentId) ", nativeQuery = true)
     List<Course> getCoursesByStudentId(@Param("studentId") Long studentId);
--- src/main/java/com/javacorner/admin/dao/StudentDAO.java
+++ src/main/java/com/javacorner/admin/dao/StudentDAO.java
@@ -1,9 +1,17 @@
 package com.javacorner.admin.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.javacorner.admin.entity.Student;
 
 public interface StudentDAO extends JpaRepository<Student, Long> {
+    @Query(value = "select i from Student as i where i.firstName like %:name% or i.lastName like %:name%")
+    List<Student> findStudentsByName(@Param("name") String name);
 
+    @Query(value = "select i from Student as i where i.user.email=:email")
+    Student findStudentByEmail(@Param("email") String email);
 }
